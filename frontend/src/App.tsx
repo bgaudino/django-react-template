10573a793b6888002f72bfcfc4237ed0d62cd394
bgaudino <bgaudino@gmail.com>
2022-05-13 10:21:22 -0500

    Add authenticated and unauthenticated routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import {useWhoAmI} from './api/queries';
 
 import Home from './routes/Home';
 import Login from './routes/Login';
@@ -7,13 +13,28 @@ import Logout from './routes/Logout';
 import Register from './routes/Register';
 
 function App() {
+  const {data, isLoading, error} = useWhoAmI();
+
+  if (isLoading) return null;
+  if (error) return <div>Error!</div>;
+
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/logout" element={<Logout />} />
         <Route path="/" element={<Home />} />
+        {data.is_authenticated ? (
+          <>
+            <Route path="/protected" element={<h1>Protected</h1>} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
--- frontend/src/routes/Logout.tsx
+++ frontend/src/routes/Logout.tsx
@@ -3,15 +3,22 @@ import {useMutation} from 'react-query';
 import {useNavigate} from 'react-router-dom';
 
 import queryClient from '../api/queryClient';
-import {logout} from '../api/queries';
+import {logout, useWhoAmI} from '../api/queries';
 
 export default function Logout() {
+  const {data, isLoading} = useWhoAmI();
+
   const navigate = useNavigate();
   const logoutMutation = useMutation(() => logout(), {
     onSuccess: () => {
       queryClient.invalidateQueries(['whoami']);
-      navigate('/login');
     },
+  });
+
+  React.useEffect(() => {
+    if (!data.is_authenticated && !isLoading) {
+      navigate('/login');
+    }
   });
 
   React.useEffect(() => logoutMutation.mutate(), []);
